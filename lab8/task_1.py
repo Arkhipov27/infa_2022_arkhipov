51518b9cfd9ef170594d52e7bb70a5d357e8d12e
Arkhipov27 <arkhipov.as@phystech.edu>
2022-10-31 14:56:01 +0300

    Первая версия игры

--- lab8/task_1.py
+++ lab8/task_1.py
@@ -10,17 +10,42 @@ y_center = 200
 
 
 def body(color, board_color, radius, board_thickness):
+    '''
+    Рисует тело смайлика
+    color: цвет тела
+    board_color: цвет границы
+    radius: радиус тела
+    board_thickness: толщина границы
+    '''
     circle(screen, color, (x_center, y_center), radius)
     circle(screen, board_color, (x_center, y_center), radius, board_thickness)
 
 
 def mouth(color, distance_from_center, height_from_center, height):
+    '''
+    Рисует рот смайлика
+    color: цвет рта
+    distance_from_center: расстояние левого верхнего угла от центра по оси x
+    height_from_center: расстояние левого верхнего угла от центра по оси y
+    height: высота рта
+    '''
     rect(screen, color, (x_center - distance_from_center, y_center + height_from_center, 2*distance_from_center,
                          height))
 
 
 def eye(color, pupil_color, board_color, distance_from_center, height_from_center, radius, pupil_radius,
         board_thickness):
+    '''
+    Рисует глаз смайлика
+    color: цвет самого глаза
+    pupil_color: цвет зрачка
+    board_color: цвет границы
+    distance_from_center: расстояние от центра смайлика до центра глаза по оси x
+    height_from_center: расстояние от центра смайлика до центра глаза по оси y
+    radius: радиус глаза
+    pupil_radius: радиус зрачка
+    board_thickness: толщина границы
+    '''
     circle(screen, color, (x_center + distance_from_center, y_center - height_from_center), radius)
     circle(screen, board_color, (x_center + distance_from_center, y_center - height_from_center), radius,
            board_thickness)
@@ -30,6 +55,15 @@ def eye(color, pupil_color, board_color, distance_from_center, height_from_cente
 
 
 def eyebrow(color, x_start, y_start, x_finish, y_finish, thickness):
+    '''
+    Рисует бровь
+    color: цвет брови
+    x_start: начальная точка по оси x
+    y_start: начальная точка по оси y
+    x_finish: конечная точка по оси x
+    y_finish: конечная точка по оси y
+    thickness: толщина брови
+    '''
     line(screen, color, (x_start, y_start), (x_finish, y_finish), thickness)
 
 
