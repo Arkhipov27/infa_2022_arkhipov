450430bea42380af4c70a98a28d7578bdd7cb667
Arkhipov27 <arkhipov.as@phystech.edu>
2022-11-06 00:08:45 +0300

    Добавлены квадраты

--- lab8/game.py
+++ lab8/game.py
@@ -1,17 +1,20 @@
 import numpy as np
 import pygame
 from pygame.draw import *
+
 pygame.init()
+pygame.font.init()
 
 
 class Ball:
     '''
     Класс, создающий мячик и обрабатывающий клики мышки
     '''
+
     def __init__(self, screen_color, color_1, color_2, color_3, color_4, color_5, color_6, fps, screen_width,
-                 screen_height, max_speed, frequency):
+                 screen_height, min_radius, max_radius, max_speed, max_ball, frequency, min_length, max_length):
         '''
-        Инициализация класса Ball
+        Конструктор класса Ball
         screen_color: цвет экрана
         color_1: первый цвет
         color_2: второй цвет
@@ -22,8 +25,12 @@ class Ball:
         fps: количество кадров в секунду
         screen_width: ширина экрана
         screen_height: высота экрана
+        min_radius: минимальный радиус мячика
+        max_radius: максимальный радиус мячика
         max_speed: максимальная скорость мячика
+        max_ball: максимальое количество мячиков на экране
         frequency: частота смены мячиков
+        length: длина стороны квадрата
         '''
         self.screen_width = screen_width
         self.screen_height = screen_height
@@ -31,201 +38,135 @@ class Ball:
         self.x = 0
         self.y = 0
         self.r = 0
+        self.square_x = 0
+        self.square_y = 0
         self.COLORS = [color_1, color_2, color_3, color_4, color_5, color_6]
         self.color = self.COLORS[np.random.randint(0, 5)]
+        self.square_color = self.COLORS[np.random.randint(0, 5)]
         self.FPS = fps
         self.finished = False
         self.click_amount = 0  # количество попыток
-        self.get_amount = 0  # количество попаданий
+        self.get_amount = 0  # количество попаданий по мячикам
         self.attempt_amount = 0  # количество кликов
+        self.square_amount = 0  # количество попаданий по квадратам
+        self.score = 0  # общий счет
         self.clock = pygame.time.Clock()
         self.screen_color = screen_color
         self.frequency = frequency
+        self.min_length = min_length
+        self.max_length = max_length
+        self.min_radius = min_radius
+        self.max_radius = max_radius
         self.max_speed = max_speed
+        self.max_ball = max_ball
+        self.speed = []  # скорость мячика
+        self.square_speed = []
         self.coord_list = []  # список координат для функции создания мячика
-
-        # параметры (флаги) для отражения от стен
-        self.fl1, self.fl2, self.fl3, self.fl4, self.fl5, self.fl6, self.fl7, self.fl8 = 0, 0, 0, 0, 0, 0, 0, 0
-        self.f1, self.f2, self.f3, self.f4 = 0, 0, 0, 0
+        self.square_list = []  # список координат для функции создания квадрата
+        self.font = pygame.font.SysFont('Arial', 30)  # шрифт для вывода количества попаданий
         pygame.display.update()
 
     def new_ball(self, ball_amount):
         '''
-        Создает новый мячик
+        Создает новые мячики
         ball_amount: количество мячиков
         '''
-        for i in range(ball_amount):
+        for k in range(ball_amount):
             self.x = np.random.randint(100, self.screen_width - 100)
             self.y = np.random.randint(100, self.screen_height - 100)
-            self.r = np.random.randint(10, 100)
+            self.r = np.random.randint(self.min_radius, self.max_radius)
             self.color = self.COLORS[np.random.randint(0, 5)]
             circle(self.screen, self.color, (self.x, self.y), self.r)
-            self.coord_list.append(self.x)
-            self.coord_list.append(self.y)
-            self.coord_list.append(self.r)
+            self.coord_list.append([self.x, self.y, self.r, self.color])
 
-    def wall_reflection(self, speed_x, speed_y):
+    def wall_reflection(self, j):
         '''
         Описывает отражение мячиков от стен
-        speed_x: скорость мячика по оси x
-        speed_y: скорость мячика по оси y
         '''
-        if speed_x > 0 and speed_y > 0:
-            if self.x + self.r <= self.screen_width and self.y + self.r <= self.screen_height and self.fl1 == 0:
-                self.x += speed_x
-                self.y += speed_y
-            elif self.x + self.r >= self.screen_width and self.y + self.r <= self.screen_height and self.fl2 == 0:
-                self.fl1 = 1
-                self.f1 = 1
-                self.x -= speed_x
-                self.y += speed_y
-            elif self.x + self.r <= self.screen_width and self.y + self.r >= self.screen_height and self.fl2 == 0:
-                self.fl1 = 1
-                self.f1 = 2
-                self.x += speed_x
-                self.y -= speed_y
-            elif self.x + self.r >= self.screen_width and self.y + self.r >= self.screen_height and self.fl2 == 0:
-                self.fl1 = 1
-                self.f1 = 3
-                self.x -= speed_x
-                self.y -= speed_y
-            elif self.x + self.r <= self.screen_width and self.y + self.r <= self.screen_height and self.f1 == 1:
-                self.fl2 = 1
-                self.x -= speed_x
-                self.y += speed_y
-            elif self.x + self.r <= self.screen_width and self.y + self.r <= self.screen_height and self.f1 == 2:
-                self.fl2 = 1
-                self.x += speed_x
-                self.y -= speed_y
-            elif self.x + self.r <= self.screen_width and self.y + self.r <= self.screen_height and self.f1 == 3:
-                self.fl2 = 1
-                self.x -= speed_x
-                self.y -= speed_y
-        if speed_x > 0 and speed_y < 0:
-            if self.x + self.r <= self.screen_width and self.y - self.r >= 0 and self.fl3 == 0:
-                self.x += speed_x
-                self.y += speed_y
-            elif self.x + self.r >= self.screen_width and self.y - self.r >= 0 and self.fl4 == 0:
-                self.fl3 = 1
-                self.f2 = 1
-                self.x -= speed_x
-                self.y += speed_y
-            elif self.x + self.r <= self.screen_width and self.y - self.r <= 0 and self.fl4 == 0:
-                self.fl3 = 1
-                self.f2 = 2
-                self.x += speed_x
-                self.y -= speed_y
-            elif self.x + self.r >= self.screen_width and self.y - self.r <= 0 and self.fl4 == 0:
-                self.fl3 = 1
-                self.f2 = 3
-                self.x -= speed_x
-                self.y -= speed_y
-            elif self.x + self.r <= self.screen_width and self.y - self.r >= 0 and self.f2 == 1:
-                self.fl4 = 1
-                self.x -= speed_x
-                self.y += speed_y
-            elif self.x + self.r <= self.screen_width and self.y - self.r >= 0 and self.f2 == 2:
-                self.fl4 = 1
-                self.x += speed_x
-                self.y -= speed_y
-            elif self.x + self.r <= self.screen_width and self.y - self.r >= 0 and self.f2 == 3:
-                self.fl4 = 1
-                self.x -= speed_x
-                self.y -= speed_y
-        if speed_x < 0 and speed_y > 0:
-            if self.x - self.r >= 0 and self.y + self.r <= self.screen_height and self.fl5 == 0:
-                self.x += speed_x
-                self.y += speed_y
-            elif self.x - self.r <= 0 and self.y + self.r <= self.screen_height and self.fl6 == 0:
-                self.fl5 = 1
-                self.f3 = 1
-                self.x -= speed_x
-                self.y += speed_y
-            elif self.x - self.r >= 0 and self.y + self.r >= self.screen_height and self.fl6 == 0:
-                self.fl5 = 1
-                self.f3 = 2
-                self.x += speed_x
-                self.y -= speed_y
-            elif self.x - self.r <= 0 and self.y + self.r >= self.screen_height and self.fl6 == 0:
-                self.fl5 = 1
-                self.f3 = 3
-                self.x -= speed_x
-                self.y -= speed_y
-            elif self.x - self.r >= 0 and self.y + self.r <= self.screen_height and self.f3 == 1:
-                self.fl6 = 1
-                self.x -= speed_x
-                self.y += speed_y
-            elif self.x - self.r >= 0 and self.y + self.r <= self.screen_height and self.f3 == 2:
-                self.fl6 = 1
-                self.x += speed_x
-                self.y -= speed_y
-            elif self.x - self.r >= 0 and self.y + self.r <= self.screen_height and self.f3 == 3:
-                self.fl6 = 1
-                self.x -= speed_x
-                self.y -= speed_y
-        if speed_x < 0 and speed_y < 0:
-            if self.x - self.r >= 0 and self.y - self.r >= 0 and self.fl7 == 0:
-                self.x += speed_x
-                self.y += speed_y
-            elif self.x - self.r <= 0 and self.y - self.r >= 0 and self.fl8 == 0:
-                self.fl7 = 1
-                self.f4 = 1
-                self.x -= speed_x
-                self.y += speed_y
-            elif self.x - self.r >= 0 and self.y - self.r <= 0 and self.fl8 == 0:
-                self.fl7 = 1
-                self.f4 = 2
-                self.x += speed_x
-                self.y -= speed_y
-            elif self.x - self.r <= 0 and self.y - self.r <= 0 and self.fl8 == 0:
-                self.fl7 = 1
-                self.f4 = 3
-                self.x -= speed_x
-                self.y -= speed_y
-            elif self.x - self.r >= 0 and self.y - self.r >= 0 and self.f4 == 1:
-                self.fl8 = 1
-                self.x -= speed_x
-                self.y += speed_y
-            elif self.x - self.r >= 0 and self.y - self.r >= 0 and self.f4 == 2:
-                self.fl8 = 1
-                self.x += speed_x
-                self.y -= speed_y
-            elif self.x - self.r >= 0 and self.y - self.r >= 0 and self.f4 == 3:
-                self.fl8 = 1
-                self.x -= speed_x
-                self.y -= speed_y
-        if self.x + self.r >= self.screen_width or self.y + self.r >= self.screen_height or self.x - self.r <= 0 \
-                or self.x - self.r <= 0:
-            self.fl1, self.fl2, self.fl3, self.fl4, self.fl5, self.fl6, self.fl7, self.fl8 = 0, 0, 0, 0, 0, 0, 0, 0
-            self.f1, self.f2, self.f3, self.f4 = 0, 0, 0, 0
+        self.coord_list[j][0] += self.speed[j][0]
+        self.coord_list[j][1] += self.speed[j][1]
+        if self.coord_list[j][0] + self.coord_list[j][2] >= self.screen_width:
+            self.speed[j][0] -= np.abs(2 * self.speed[j][0])
+        if self.coord_list[j][1] + self.coord_list[j][2] >= self.screen_height:
+            self.speed[j][1] -= np.abs(2 * self.speed[j][1])
+        if self.coord_list[j][0] - self.coord_list[j][2] <= 0:
+            self.speed[j][0] += np.abs(2 * self.speed[j][0])
+        if self.coord_list[j][1] - self.coord_list[j][2] <= 0:
+            self.speed[j][1] += np.abs(2 * self.speed[j][1])
+
+    def new_square(self, ball_amount):
+        '''
+        Создает новые квадраты
+        ball_amount: количество квадратов
+        '''
+        length = np.random.randint(self.min_length, self.max_length)
+        for k in range(ball_amount):
+            self.square_x = np.random.randint(100, self.screen_width - 100)
+            self.square_y = np.random.randint(100, self.screen_height - 100)
+            self.square_color = self.COLORS[np.random.randint(0, 5)]
+            rect(self.screen, self.color, (self.square_x - length // 2, self.square_y - length // 2,
+                                           length, length))
+            self.square_list.append([self.square_x, self.square_y, self.square_color, length])
 
     def click(self):
         '''
         Обрабатывает клики мышки
         '''
         while not self.finished:
-            speed_x = np.random.randint(-self.max_speed, self.max_speed)
-            speed_y = np.random.randint(-self.max_speed, self.max_speed)
+            text = self.font.render('{}'.format(self.score), True, (255, 255, 255))
+            self.speed = []
+            self.coord_list = []
+            self.new_ball(np.random.randint(1, self.max_ball))
+            for j in range(len(self.coord_list)):
+                self.speed.append([np.random.randint(-self.max_speed, self.max_speed),
+                                   np.random.randint(-self.max_speed, self.max_speed)])
+            self.square_speed = []
+            self.square_list = []
+            self.new_square(np.random.randint(1, self.max_ball))
+            for j in range(len(self.square_list)):
+                self.square_speed.append([np.random.randint(-self.max_speed//5, self.max_speed//5),
+                                          np.random.randint(-self.max_speed, self.max_speed)])
             self.attempt_amount += 1
-            self.new_ball(np.random.randint(1, 5))
-            for i in range(self.FPS//self.frequency):
+            for i in range(self.FPS // self.frequency):
                 self.clock.tick(self.FPS)
                 for event in pygame.event.get():
                     if event.type == pygame.QUIT:
                         self.finished = True
                         print('Количество попыток: ', self.attempt_amount)
                         print('Количество кликов: ', self.click_amount)
-                        print('Количество попаданий: ', self.get_amount)
+                        print('Количество попаданий в мячики: ', self.get_amount)
+                        print('Количество попаданий в квадраты: ', self.square_amount)
+                        print('Счет: ', self.score**3 / (self.attempt_amount * self.click_amount))
                     elif event.type == pygame.MOUSEBUTTONDOWN:
                         self.click_amount += 1
-                        if (self.x - pygame.mouse.get_pos()[0]) ** 2 + (self.y - pygame.mouse.get_pos()[1]) ** 2\
-                                <= self.r ** 2:
-                            self.get_amount += 1
-                            print('Goal!')
-                self.wall_reflection(speed_x, speed_y)
-                circle(self.screen, self.color, (self.x, self.y), self.r)
+                        for j in range(len(self.coord_list)):
+                            if (self.coord_list[j][0] - pygame.mouse.get_pos()[0]) ** 2 \
+                                    + (self.coord_list[j][1] - pygame.mouse.get_pos()[1]) ** 2 \
+                                    <= self.coord_list[j][2] ** 2:
+                                self.score += 1
+                                self.get_amount += 1
+                                print('Goal!')
+                        for t in range(len(self.square_list)):
+                            if (self.square_list[t][0] - self.square_list[t][3] <= pygame.mouse.get_pos()[0]) and \
+                               (self.square_list[t][0] + self.square_list[t][3] >= pygame.mouse.get_pos()[0]) and \
+                                    (self.square_list[t][1] - self.square_list[t][3] <= pygame.mouse.get_pos()[1]) and \
+                                    (self.square_list[t][1] + self.square_list[t][3] >= pygame.mouse.get_pos()[1]):
+                                self.score += 3
+                                self.square_amount += 1
+                                print('Goal!')
+                for j in range(len(self.coord_list)):
+                    self.wall_reflection(j)
+                    circle(self.screen, self.coord_list[j][3], (self.coord_list[j][0], self.coord_list[j][1]),
+                           self.coord_list[j][2])
+                for j in range(len(self.square_list)):
+                    self.square_list[j][0] += self.square_speed[j][0]*np.abs(np.sin(self.square_list[j][0]))
+                    self.square_list[j][1] += self.square_speed[j][1]*np.abs(np.sin(self.square_list[j][1]))
+                    rect(self.screen, self.square_list[j][2],
+                         (self.square_list[j][0] - self.square_list[j][3] // 2, self.square_list[j][1] - self.square_list[j][3] // 2,
+                          self.square_list[j][3], self.square_list[j][3]))
                 pygame.display.update()
                 self.screen.fill(self.screen_color)
+                self.screen.blit(text, (0, 0))
 
 
 def main():
@@ -236,7 +177,7 @@ def main():
     MAGENTA = (255, 0, 255)
     CYAN = (0, 255, 255)
     BLACK = (0, 0, 0)
-    ball = Ball(BLACK, RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, 60, 1200, 800, 20, 1)
+    ball = Ball(BLACK, RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN, 60, 1200, 800, 10, 100, 20, 5, 1, 20, 50)
     ball.click()
 
 
